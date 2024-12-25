6e9c754360a041ee56c9375c4de2615611516a4d
Turja Chakraborty <152412436+Turjacb1@users.noreply.github.com>
2024-12-26 00:47:10 +0600

    add hak,leet,bee

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,6 +3,7 @@ import './About.css';
 import myimage from '../Image/me.jpg';
 import { FaFacebook, FaLinkedin, FaTwitter,FaBeer } from 'react-icons/fa';
 import { FaArrowRight } from 'react-icons/fa'; // Import the arrow icon
+import { SiHackerrank, SiLeetcode, SiCodeforces } from 'react-icons/si'; // For HackerRank, LeetCode, and Beecrowd
 
 const About = () => (
   <section id="about">
@@ -11,33 +12,50 @@ const About = () => (
       <h2>Student of Computer Science And Engineering</h2>
       
       <div className="social-buttons">
-  <div className="social-icon-container">
-    <div className="dancing-arrow"><FaArrowRight /></div>
-    <a href="https://web.facebook.com/turja.chakraborty.52/" target="_blank" rel="noopener noreferrer">
-      <FaFacebook className="social-icon" />
-      <span className="tooltip">Facebook</span>
-    </a>
-  </div>
-  <div className="social-icon-container">
-    <a href="https://www.linkedin.com/in/turja-chakraborty-258125248/" target="_blank" rel="noopener noreferrer">
-      <FaLinkedin className="social-icon" />
-      <span className="tooltip">LinkedIn</span>
-    </a>
-  </div>
-  <div className="social-icon-container">
-    <a href="https://x.com/Turjackb" target="_blank" rel="noopener noreferrer">
-      <FaTwitter className="social-icon" />
-      <span className="tooltip">Twitter</span>
-    </a>
-  </div>
-  <div className="social-icon-container">
-    <a href="https://github.com/Turjacb1" target="_blank" rel="noopener noreferrer">
-      <FaBeer className="social-icon" />
-      <span className="tooltip">GitHub</span>
-    </a>
-  </div>
-</div>
-
+        <div className="social-icon-container">
+          <div className="dancing-arrow"><FaArrowRight /></div>
+          <a href="https://web.facebook.com/turja.chakraborty.52/" target="_blank" rel="noopener noreferrer">
+            <FaFacebook className="social-icon" />
+            <span className="tooltip">Facebook</span>
+          </a>
+        </div>
+        <div className="social-icon-container">
+          <a href="https://www.linkedin.com/in/turja-chakraborty-258125248/" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin className="social-icon" />
+            <span className="tooltip">LinkedIn</span>
+          </a>
+        </div>
+        <div className="social-icon-container">
+          <a href="https://x.com/Turjackb" target="_blank" rel="noopener noreferrer">
+            <FaTwitter className="social-icon" />
+            <span className="tooltip">Twitter</span>
+          </a>
+        </div>
+        <div className="social-icon-container">
+          <a href="https://github.com/Turjacb1" target="_blank" rel="noopener noreferrer">
+            <FaBeer className="social-icon" />
+            <span className="tooltip">GitHub</span>
+          </a>
+        </div>
+        <div className="social-icon-container">
+          <a href="https://www.hackerrank.com/profile/turjackb28" target="_blank" rel="noopener noreferrer">
+            <SiHackerrank className="social-icon" />
+            <span className="tooltip">HackerRank</span>
+          </a>
+        </div>
+        <div className="social-icon-container">
+          <a href="https://leetcode.com/u/ijFQrCWjE6/" target="_blank" rel="noopener noreferrer">
+            <SiLeetcode className="social-icon" />
+            <span className="tooltip">LeetCode</span>
+          </a>
+        </div>
+        <div className="social-icon-container">
+          <a href="https://judge.beecrowd.com/en/profile/853727" target="_blank" rel="noopener noreferrer">
+            <SiCodeforces className="social-icon" />
+            <span className="tooltip">Beecrowd</span>
+          </a>
+        </div>
+      </div>
     </div>
     <img src={myimage} alt="Turja Chakraborty" className="profile-img" />
   </section>
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -7,6 +7,8 @@ import project4 from '../Image/4pro.png';
 import project5 from '../Image/5pro.png';
 import project6 from '../Image/6pro.png';
 import project7 from '../Image/7pro.png';
+import project8 from '../Image/8pro.png';
+
 
 const Portfolio = () => {
   const initialVisibleProjects = 3;
@@ -17,10 +19,11 @@ const Portfolio = () => {
     { id: 1, src: project1, alt: 'Project 1', link: 'https://doctors-portal-ad.firebaseapp.com/', title: 'Doctors-Portal' },
     { id: 2, src: project2, alt: 'Project 2', link: 'https://ama-john-second.web.app/', title: 'Shopping' },
     { id: 3, src: project7, alt: 'Project 3', link: 'https://air-agency-ec79e.web.app/', title: ' Air-Agency' },
-    { id: 4, src: project4, alt: 'Project 4', link: 'https://link-to-project4.com', title: 'Top Businessmen' },
-    { id: 5, src: project5, alt: 'Project 5', link: 'https://link-to-project5.com', title: 'Country Information' },
-    { id: 6, src: project6, alt: 'Project 6', link: 'https://link-to-project6.com', title: 'Pin Matcher' },
-    { id: 7, src: project3, alt: 'Project 7', link: 'https://link-to-project7.com', title: 'Money Transaction' }
+    { id: 4, src: project8, alt: 'Project 4', link: 'https://calculator-f1d27.firebaseapp.com/', title: ' Calculator' },
+    { id: 5, src: project4, alt: 'Project 5', link: 'https://link-to-project4.com', title: 'Top Businessmen' },
+    { id: 6, src: project5, alt: 'Project 6', link: 'https://link-to-project5.com', title: 'Country Information' },
+    { id: 7, src: project6, alt: 'Project 7', link: 'https://link-to-project6.com', title: 'Pin Matcher' },
+    { id: 8, src: project3, alt: 'Project 8', link: 'https://link-to-project7.com', title: 'Money Transaction' }
   ];
 
   // Handle "View More" click to show more projects
